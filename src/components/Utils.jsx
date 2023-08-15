d69094c80b94fb7786ccf72082d8725c65a8cf83
scragonrex <scragondragon@gmail.com>
2023-08-15 11:37:00 +0530

    completed the modified select component

--- src/components/Utils.jsx
+++ src/components/Utils.jsx
@@ -1,6 +1,7 @@
-import { InputLabel, OutlinedInput, Popover, Select, styled } from '@mui/material';
+import { ClickAwayListener, InputLabel, OutlinedInput, Popover, Select, styled } from '@mui/material';
 import { useState } from 'react';
-import '../styles/Utils.css'
+import '../styles/Utils.css';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 const SelectX = styled(Select)(
   {
     color: "rgb(6, 207, 106) !important",
@@ -47,64 +48,53 @@ const OutlinedInputX = styled(OutlinedInput)(
   }
 )
 
-const SelectCont = styled('div')(
-  {
-    padding: "1rem",
-    border: "1.5px solid rgb(6, 207, 106)",
-    color: "white",
-    borderRadius: "5px",
-    cursor: "pointer"
-  }
-)
 
-const OptionCont = styled('div')({
-  position: "absolute",
-  width: "100%",
-  height: "0",
-  borderRadius: "0 0 5px 5px",
-  cursor: "pointer",
-  backgroundColor: "#121212",
-  overflow: "auto",
-  transition: "0.3s",
-})
-
-const Option = styled('div')({
-  padding: "0.5rem 1rem",
-  color: "white",
-   cursor: "pointer",
-   "&:hover":{
-    backgroundColor:"#181819",
-   },
- 
-
-})
 
 const SelectMod = ({ options, title }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [selectValue, setSelectValue] = useState(title);
-  const handleChange = (e) => {
+
+  const handleChange = (e,reason) => {
+    
     setSelectValue(e.currentTarget.textContent);
-    const id = document.getElementById("optionCont");
-    id.style.height="0";
-  }
-  const handleSelect = () => {
-    const id = document.getElementById("optionCont");
+    const id = document.getElementById("optionCont1");
+    const icon = document.getElementById("expandIcon");
+
+    // if(reason==="clickaway");
+    // {
+    // return;
+    // }
+    icon.classList.toggle("rotate");
     id.classList.toggle("expanded");
   }
-  
+  const handleSelect = () => {
+    const id = document.getElementById("optionCont1");
+    const icon = document.getElementById("expandIcon");
+    icon.classList.toggle("rotate");
+    id.classList.toggle("expanded");
+  }
+  const handleClickAway = ()=>{
+    const id = document.getElementById("optionCont1");
+    const icon = document.getElementById("expandIcon");
+    icon.classList.remove("rotate");
+    id.classList.remove("expanded");
+  }
 
 
   return (
 
+      <ClickAwayListener onClickAway={handleClickAway}>
     <div style={{ position: "relative" }}>
-      <SelectCont onClick={handleSelect}>{selectValue}</SelectCont>
-      <OptionCont id="optionCont" >
+      <div className='selectCont' onClick={handleSelect}>{selectValue} <div id='expandIcon'><ExpandMoreIcon /></div> </div>
+      <div className='optionCont' id="optionCont1" >
         {options?.map((item, key) => (
-          <Option id={key} value={item} onClick={handleChange}>{item}</Option>
-        ))
+          <div className='option' id={key} value={item} onClick={handleChange}>{item}</div>
+          ))
         }
-      </OptionCont>
+      </div>
+      
     </div>
+        </ClickAwayListener>
   )
 }
 export { InputLabelX, SelectX, OutlinedInputX, SelectMod };
