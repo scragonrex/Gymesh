e99571aa17855ac7081f90e40c99f1e82bb7a7b0
scragonrex <scragondragon@gmail.com>
2023-08-24 20:32:51 +0530

    refactored the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import "../styles/Footer.css"
-import { AccountCircleOutlined, FlagCircleOutlined, GitHub, Instagram, LinkedIn, ManageSearchRounded, WhatsApp } from '@mui/icons-material';
+import { AccountCircleOutlined, Facebook, FlagCircleOutlined, GitHub, Instagram, LinkedIn, ManageSearchRounded, WhatsApp } from '@mui/icons-material';
 import { Tooltip } from '@mui/material';
 const Footer = () => {
   return (
     <div>
         <footer className='footer'>
-            <div className='font-1'>Created with Love and Passion</div>
-            <div className='margin-top-1 font-1'>Contact Us</div>
+            <div className='font-1'>Muscle Grabber</div>
             <div >scragondragon@gmail.com</div>
             <div className="iconsCont">
-              <GitHub/>
+              <a href="https://github.com/scragonrex/Gymesh-frontend" target="_blank"><GitHub/></a>
+              <a href="https://www.linkedin.com/in/abhishek-anand-a55028208/" target="_blank"><LinkedIn/></a>
+              <a href="https://www.facebook.com/profile.php?id=100012831764394" target="_blank"><Facebook/></a>
               <Instagram/>
-              <LinkedIn/>
               <Tooltip title="6394323721">
               <WhatsApp/>
               </Tooltip>
