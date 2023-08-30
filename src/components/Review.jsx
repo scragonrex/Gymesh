aff9718d04d4aa3b7275fe8829bff2478c445a6f
scragonrex <scragondragon@gmail.com>
2023-08-30 10:30:46 +0530

    working on review slider

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,23 +1,53 @@
 import React from 'react'
 import "../styles/Review.css"
 import Avatar from '@mui/material/Avatar';
+import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 const Review = () => {
   return (
     <div className='reviewCont'>
         <div>
-            <p className="font-5 font-heading">What are Clients have to say?</p>
-            <p className='font-grey'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quo autem ipsum sequi tempore nihil placeat rem quos tempora perferendis in temporibus harum.</p>
+            <p className="font-5 text-align-center font-heading">What are Clients have to say?</p>
+            <p className='font-grey text-align-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quo autem ipsum sequi tempore nihil placeat rem quos tempora perferendis in temporibus harum.</p>
         </div>
-        <div>
+        <div className='reviewCardCont'>
             <div className="reviewCard">
                 <div className="display-flex-row align-item-center gap-2">
-               <Avatar sx={{width:60, height:60, bgcolor:"purple"}}>H</Avatar>
+               <Avatar sx={{width:60, height:60, bgcolor:"purple", fontSize:"2rem"}}>H</Avatar>
                <div>
                <div className="font-2">Scragon Rex</div>
                <div className="font-grey">Scragon Rex</div>
                </div>
                 </div>
                 <p className="font-grey margin-top-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere harum repellat at minus </p>
+            </div>
+            <div className="reviewCard">
+                <div className="display-flex-row align-item-center gap-2">
+               <Avatar sx={{width:60, height:60, bgcolor:"purple", fontSize:"2rem"}}>H</Avatar>
+               <div>
+               <div className="font-2">Scragon Rex</div>
+               <div className="font-grey">Scragon Rex</div>
+               </div>
+                </div>
+                <p className="font-grey margin-top-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere harum repellat at minus </p>
+            </div>
+            <div className="reviewCard">
+                <div className="display-flex-row align-item-center gap-2">
+               <Avatar sx={{width:60, height:60, bgcolor:"purple", fontSize:"2rem"}}>H</Avatar>
+               <div>
+               <div className="font-2">Scragon Rex</div>
+               <div className="font-grey">Scragon Rex</div>
+               </div>
+                </div>
+                <p className="font-grey margin-top-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere harum repellat at minus </p>
+            </div>
+
+            <div className='cardLeft'>
+            <ArrowCircleLeftOutlinedIcon sx={{fontSize:"2rem"}} />
+            </div>
+
+            <div  className='cardRight'>
+            <ArrowCircleRightOutlinedIcon sx={{fontSize:"2rem"}} />
             </div>
         </div>
     </div>
