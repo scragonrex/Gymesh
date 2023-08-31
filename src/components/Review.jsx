bc62fc372ef9988537a6102cf14eab2575af20d6
scragonrex <scragondragon@gmail.com>
2023-08-31 19:53:07 +0530

    added slider indicators in review componenet

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -3,11 +3,14 @@ import "../styles/Review.css"
 import Avatar from '@mui/material/Avatar';
 import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
+import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
 const Review = () => {
 
     const [activeIndex, setActiveIndex] = useState(0);
 
     const handleReview = (value) => {
+       
         const size = index.length - 3;
         if (value < 0)
             setActiveIndex(size);
@@ -53,40 +56,51 @@ const Review = () => {
         designation: "Web Developer",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere harum repellat at minus "
     },
-    ]
+    ];
+   
     return (
         <div className='reviewCont'>
             <div>
                 <p className="font-5 text-align-center font-heading">What are Clients have to say?</p>
                 <p className='font-grey text-align-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quo autem ipsum sequi tempore nihil placeat rem quos tempora perferendis in temporibus harum.</p>
             </div>
+    
+           
             <div className='reviewCardCont'>
-                <div className="swipeWindow" style={{ transform: `translateX(-${activeIndex * 34}%` }}>
+           
+           <div className="swipeWindow" style={{ transform: `translateX(-${activeIndex * 34}%` }}>
 
-                    {
-                        index?.length > 0 && index.map((item, key) => (
-                            <div id={key} className="reviewCard">
-                                <div className="display-flex-row align-item-center gap-2">
-                                    <Avatar sx={{ width: 60, height: 60, bgcolor: "purple", fontSize: "2rem" }}>{item.name[0]}</Avatar>
-                                    <div>
-                                        <div className="font-2">{item.name}</div>
-                                        <div className="font-grey">{item.designation}</div>
-                                    </div>
-                                </div>
-                                <p className="font-grey margin-top-2">{item.description}</p>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
+               {
+                   index?.length > 0 && index.map((item, key) => (
+                       <div id={key} className="reviewCard">
+                           <div className="display-flex-row align-item-center gap-2">
+                               <Avatar sx={{ width: 60, height: 60, bgcolor: "purple", fontSize: "2rem" }}>{item.name[0]}</Avatar>
+                               <div>
+                                   <div className="font-2">{item.name}</div>
+                                   <div className="font-grey">{item.designation}</div>
+                               </div>
+                           </div>
+                           <p className="font-grey margin-top-2">{item.description}</p>
+                       </div>
+                   ))
+               }
+           </div>
+           
+       </div>
+           
+            
+           
             <div className="display-flex-row gap-2 align-item-center">
-                <ArrowCircleLeftOutlinedIcon onClick={() => handleReview(activeIndex - 1)} sx={{ fontSize: "2rem" }} />
+            <ArrowCircleLeftOutlinedIcon onClick={() => handleReview(activeIndex - 1)} sx={{ fontSize: "2rem", cursor:"pointer" }} />
                 {
-                    index?.length>0 && index.map((item,key)=>(
-                        <div className='sliderDots'>.</div>
+                      
+                    index?.length>0 && index.slice(0,index.length-2).map((item,key)=>(
+                        <>{key === activeIndex ? <CircleRoundedIcon sx={{fontSize:"0.7rem"}}/> : <CircleOutlinedIcon sx={{fontSize:"0.7rem"}}/>
+                        }</>
                     ))
                 }
-                <ArrowCircleRightOutlinedIcon onClick={() => handleReview(activeIndex + 1)} sx={{ fontSize: "2rem" }} />
+                 <ArrowCircleRightOutlinedIcon onClick={() => handleReview(activeIndex + 1)} sx={{ fontSize: "2rem",cursor:"pointer" }} />
+                
             </div>
         </div>
     )
