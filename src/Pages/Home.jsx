7b04714a528bc597abc1dca68aedb111ee1a1d86
scragonrex <scragondragon@gmail.com>
2023-09-08 19:13:34 +0530

    modified the features contaier to a new component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,86 +9,42 @@ import LeaderBoard from '../components/LeaderBoard';
 import AboutUs from '../components/AboutUs';
 import Footer from '../components/Footer';
 import Review from '../components/Review';
+import Features from '../components/Features';
 const Home = () => {
   const navigate = useNavigate();
   const mobileView = useMediaQuery('(max-width:720px)');
-  const user = useSelector((state)=>state.auth.user)
+  const user = useSelector((state) => state.auth.user)
 
-  const handleSignUp = (e)=>{
+  const handleSignUp = (e) => {
     const id = document.getElementById("exploreFeature");
-    if(e.currentTarget.innerText==="Explore Us")
-    {
+    if (e.currentTarget.innerText === "Explore Us") {
       id.scrollIntoView({ behavior: 'smooth' });
     }
     else
-    navigate('/signup')
+      navigate('/signup')
   }
   return (
     <>
-     <div className='landingContainer'>
-    <div className='mainPage'>
-      <img src="/assets/background1.jpg" alt="img1" className="bgImg" />
-      <div className="pageTitle">
-        <div className={`${mobileView?"font-3":"font-6"} font-bold`}>Elevate Your Fitness Journey with Us!</div>
-        <p className=' font-1 text-align-center font-grey'>We're more than just a gym – we're a haven for individuals seeking holistic wellness and growth. Our philosophy revolves around the understanding that true transformation extends beyond the physical.
-        </p>
-        <button className="btn1" onClick={handleSignUp}><div className='font-3'>{user ? "Explore Us":"Signup Now"}</div></button>
-      </div>
-      
-    </div>
-    <div className="features">
-      <img src="/assets/featuresBg.png" alt="feature" />
-      <div id="exploreFeature" className="featuresCont">
-      <div className="display-flex-col">
-      <div className="font-white font-5 font-bold font-heading">Checkout our new Features!</div>
-      <div className="margin-top-1 font-grey">Use these features to enhance your your workout experience. Every feature has there own benefits and functionality. So have fun with playing it. </div>
-      </div>
-      <div className="featureCardCont">
-      <div className="featureCard" onClick={()=>navigate('/workout')}>
-        <div className='cardImg'>
-        <img src="/assets/c1.png" alt="c1"  />
-        </div>
-        <div>
-        <div className='font-3 font-white font-bold'>Excercise Finder</div>
-        <div className='font-grey margin-top-1'>Explore various ranges of excercise according to your various muscles</div>
-        </div>
-      </div>
+      <div className='landingContainer'>
+        <div className='mainPage'>
+          <img src="/assets/background1.jpg" alt="img1" className="bgImg" />
+          <div className="pageTitle">
+            <div className={`${mobileView ? "font-3" : "font-6"} font-bold`}>Elevate Your Fitness Journey with Us!</div>
+            <p className=' font-1 text-align-center font-grey'>We're more than just a gym – we're a haven for individuals seeking holistic wellness and growth. Our philosophy revolves around the understanding that true transformation extends beyond the physical.
+            </p>
+            <button className="btn1" onClick={handleSignUp}><div className='font-3'>{user ? "Explore Us" : "Signup Now"}</div></button>
+          </div>
 
-      <div className="featureCard" onClick={()=>navigate('/goal')}>
-      <div className='cardImg'>
-      <img src="/assets/c2.png" alt="c2"  />
-      </div>
-        <div>
-        <div className='font-3 font-white font-bold'>Goal Maker</div>
-        <div className='font-grey margin-top-1'>Create a weeekly plan for your Workouts and gain muscle scores after completion of the goals</div>
         </div>
       </div>
+      <Features/>
+      <LeaderBoard />
+      <Review />
+      <AboutUs />
+      <Footer />
 
-      <div className="featureCard" >
-      <div className='cardImg'>
-      <img src="/assets/c3.png" alt="c3"  />
-      </div>
-        <div>
-        <div className='font-3 font-white font-bold'>Comming Soon</div>
-        <div className='font-grey margin-top-1'>Currently working on the new feature. </div>
-        </div>
-      </div>
-     
-     
-     
-      </div>
-      </div>
-    </div>
-
-   
-    </div>
-    <LeaderBoard/> 
-    <Review/>
-    <AboutUs/>
-    <Footer/>
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
     </>
 
   )
---
+++ src/components/Features.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import "../styles/Features.css"
+const Features = () => {
+    const navigate = useNavigate();
+  return (
+    <div className="features">
+      <img src="/assets/featuresBg.png" alt="feature" />
+      <div id="exploreFeature" className="featuresCont">
+      <div className="display-flex-col">
+      <div className="font-white font-5 font-bold font-heading">Checkout our new Features!</div>
+      <div className="margin-top-1 font-grey">Use these features to enhance your your workout experience. Every feature has there own benefits and functionality. So have fun with playing it. </div>
+      </div>
+      <div className="featureCardCont">
+      <div className="featureCard" onClick={()=>navigate('/workout')}>
+        <div className='cardImg'>
+        <img src="/assets/c1.png" alt="c1"  />
+        </div>
+        <div>
+        <div className='font-3 font-white font-bold'>Excercise Finder</div>
+        <div className='font-grey margin-top-1'>Explore various ranges of excercise according to your various muscles</div>
+        </div>
+      </div>
+
+      <div className="featureCard" onClick={()=>navigate('/goal')}>
+      <div className='cardImg'>
+      <img src="/assets/c2.png" alt="c2"  />
+      </div>
+        <div>
+        <div className='font-3 font-white font-bold'>Goal Maker</div>
+        <div className='font-grey margin-top-1'>Create a weeekly plan for your Workouts and gain muscle scores after completion of the goals</div>
+        </div>
+      </div>
+
+      <div className="featureCard" >
+      <div className='cardImg'>
+      <img src="/assets/c3.png" alt="c3"  />
+      </div>
+        <div>
+        <div className='font-3 font-white font-bold'>Comming Soon</div>
+        <div className='font-grey margin-top-1'>Currently working on the new feature. </div>
+        </div>
+      </div>
+     
+     
+     
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Features
