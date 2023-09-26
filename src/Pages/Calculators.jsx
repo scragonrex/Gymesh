5efab9b18926e8a972e3b82af0b8eeb18c6e3c30
scragonrex <scragondragon@gmail.com>
2023-09-26 09:37:58 +0530

    added calculator page and changed the btn UI

--- src/Pages/Calculators.jsx
+++ src/Pages/Calculators.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import "../styles/Calculators.css"
 const Calculators = () => {
   return (
-    <div>
+    <div className='calculators'>
       Calculators
     </div>
   )
 }
-
+ 
 export default Calculators
